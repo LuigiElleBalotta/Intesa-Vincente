92b3249e709702cbc88f8c6630e02203d0380c76
Luigi Balotta <l.balotta@mindicity.com>
2023-03-29 15:48:35 +0200

    Fix wordSetFile

--- src/core/singletons/words.singleton.ts
+++ src/core/singletons/words.singleton.ts
@@ -11,15 +11,19 @@ export class WordsSingleton {
     ];
     
     public static GetWordsSet( number: number ): void {
-      const path = join(__dirname, '..', 'public');
+      const path = join(__dirname, '..', '..', '..', 'public');
       const wordSetFile = `${path}/words_set_${number}.json`;
+      console.log('wordSetFile', wordSetFile);
       if( fs.existsSync(wordSetFile) ) {
-        this.words = JSON.parse(fs.readFileSync(wordSetFile, 'utf8'));
+        WordsSingleton.words = JSON.parse(fs.readFileSync(wordSetFile, 'utf8'));
+      }
+      else {
+        console.log('File not found');
       }
     }
     
     public static GetRandomWordExcluding(excludedWords: string[]): string {
-      const words: string[] = this.words.filter(w => !excludedWords.includes(w));
+      const words: string[] = WordsSingleton.words.filter(w => !excludedWords.includes(w));
       return words[Math.floor(Math.random() * words.length)];
     }
 }
